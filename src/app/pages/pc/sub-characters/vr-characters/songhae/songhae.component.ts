f9f6eed63e5eee71698eccc804cf395a19f43d8f
최홍희 <vvshinevv@nhntoast.com>
2021-07-22 00:00:34 +0900

    수정된 css적용

--- src/app/pages/pc/sub-characters/vr-characters/songhae/songhae.component.ts
+++ src/app/pages/pc/sub-characters/vr-characters/songhae/songhae.component.ts
@@ -1,31 +1,97 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from "@angular/core";
 import { RoomEnvironment } from "../../../../../../assets/jsm/environments/RoomEnvironment.js";
 import { OrbitControls } from "../../../../../../assets/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "../../../../../../assets/jsm/loaders/GLTFLoader.js";
 import { KTX2Loader } from "../../../../../../assets/jsm/loaders/KTX2Loader.js";
 import { MeshoptDecoder } from "../../../../../../assets/jsm/libs/meshopt_decoder.module.js";
 import * as THREE from "three";
+import Swiper from "swiper";
 
 @Component({
-  selector: 'app-songhae',
-  templateUrl: './songhae.component.html',
-  styleUrls: ['./songhae.component.css']
+  selector: "app-songhae",
+  templateUrl: "./songhae.component.html",
+  styleUrls: ["./songhae.component.css"],
 })
 export class SonghaeComponent implements OnInit {
   @ViewChild("target") targetElement: ElementRef;
+  @ViewChild("closeLayer") closeLayerElement: ElementRef;
+  @ViewChild("promotionMain") promotionMainElement: ElementRef;
+  @ViewChild("backToHome") backToHomeElement: ElementRef;
+  @ViewChild("profileLink") profileLinkElement: ElementRef;
+  @ViewChild("locationLink") locationLinkElement: ElementRef;
+  @ViewChild("itemLink") itemLinkElement: ElementRef;
+  @ViewChild("pcProfile") pcProfileElement: ElementRef;
+  @ViewChild("mobileProfile") mobileProfileElement: ElementRef;
+
   camera: any;
   scene: any;
   renderer: any;
   mixer: any;
+  swiper: any;
 
-  constructor() { }
+  clickProfileFlag: boolean = false;
+  clickSaeulFlag: boolean = false;
+  clickItemFlag: boolean = false;
+
+  constructor(private r2: Renderer2) {}
 
   ngOnInit() {
+    this.initSwiper();
     this.init();
     this.render(this.renderer);
+    
   }
 
-  init() {
+
+  public initSwiper() {
+    this.swiper = new Swiper(".swiper-container", {
+      slidesPerView: 1,
+      spaceBetween: 96,
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+
+  public clickCloseLayer() {
+    this.r2.removeClass(this.promotionMainElement.nativeElement, "profile");
+    this.r2.removeClass(this.profileLinkElement.nativeElement, "on");
+
+    this.r2.setStyle(this.backToHomeElement.nativeElement, "display", "");
+    this.r2.setStyle(this.closeLayerElement.nativeElement, "display", "none");
+    this.r2.setStyle(this.pcProfileElement.nativeElement, "display", "none");
+    this.r2.setStyle(
+      this.mobileProfileElement.nativeElement,
+      "display",
+      "none"
+    );
+  }
+
+  public clickProfile() {
+    this.clickProfileFlag = !this.clickProfileFlag;
+
+    this.r2.addClass(this.promotionMainElement.nativeElement, "profile");
+    this.r2.addClass(this.profileLinkElement.nativeElement, "on");
+
+    this.r2.setStyle(this.backToHomeElement.nativeElement, "display", "none");
+    this.r2.setStyle(this.closeLayerElement.nativeElement, "display", "");
+    this.r2.setStyle(this.pcProfileElement.nativeElement, "display", "");
+    this.r2.setStyle(this.mobileProfileElement.nativeElement, "display", "");
+  }
+
+  public clickSaeul() {}
+
+  public clickItem() {}
+
+  public init() {
     const container = this.targetElement.nativeElement;
     document.body.appendChild(container);
 
@@ -52,27 +118,23 @@ export class SonghaeComponent implements OnInit {
     this.scene.background = new THREE.Color(0xbbbbbb);
     this.scene.environment = pmremGenerator.fromScene(environment).texture;
 
-    // const grid = new THREE.GridHelper(500, 10, 0xffffff, 0xffffff);
-    // grid.material.opacity = 0.5;
-    // grid.material.depthWrite = false;
-    // grid.material.transparent = true;
-    // this.scene.add(grid);
-
-    var geometry = new THREE.SphereGeometry( 500, 60, 40 );
-    geometry.scale( - 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( {
-      map: new THREE.TextureLoader().load( '../../../../../../assets/models/gltf/hdri.jpg' )
-    } );
-    let mesh = new THREE.Mesh( geometry, material );
-    this.scene.add( mesh );
-
-
+    var geometry = new THREE.SphereGeometry(500, 60, 40);
+    geometry.scale(-1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load(
+        "../../../../../../assets/models/gltf/hdri.jpg"
+      ),
+    });
+    let mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(mesh);
 
     const ktx2Loader = new KTX2Loader()
       .setTranscoderPath("../../../../../../assets/js/libs/basis/")
       .detectSupport(this.renderer);
 
-    const loader = new GLTFLoader().setPath("../../../../../../assets/models/gltf/");
+    const loader = new GLTFLoader().setPath(
+      "../../../../../../assets/models/gltf/"
+    );
     loader.setKTX2Loader(ktx2Loader);
     loader.setMeshoptDecoder(MeshoptDecoder);
 
@@ -93,7 +155,7 @@ export class SonghaeComponent implements OnInit {
     window.addEventListener("resize", () => this.onWindowResize(this.renderer));
   }
 
-  onWindowResize(renderer: any) {
+  public onWindowResize(renderer: any) {
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
 
@@ -102,7 +164,7 @@ export class SonghaeComponent implements OnInit {
     this.render(renderer);
   }
 
-  render(renderer: any) {
+  public render(renderer: any) {
     renderer.render(this.scene, this.camera);
   }
 }
